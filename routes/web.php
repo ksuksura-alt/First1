d70fc842f7bc7cc0534b503caf43c1f5952152b8
Ksenia <ksushka@example.com>
2026-03-16 21:41:23 +0600

    homework

--- routes/web.php
+++ routes/web.php
@@ -1,71 +1,25 @@
-<?php
+ <?php
+use App\Http\Controllers\UserController;
 
-use Illuminate\Support\Facades\Route;
-/*  Route::get('/', function () {
-		return 'главная страница сайта';
-	});
-	
-    Route::get('/post/{id}', function ($id) {
-		return 'пост' .$id;
-	})->where('id','[0-9]+');
-	
-	Route::get('/post/{slug}/{id}', function ($slug, $id) {
-		return 'пост ' . $slug . ' ' . $id;
-	})->where('slug', '[a-z0-9_-]+')->where('id', '[0-9]+');
+Route::get('/user', [UserController::class, 'show']);
+Route::get('/user/all', [UserController::class, 'all']);
+Route::get('/user/{name}', [UserController::class, 'showByName']);
+Route::get('/user/{surname}/{name}', [UserController::class, 'showByFullName']);
 
-	Route::get('/post/1', function () {
-		return 'первый пост';
-	});
+$users = [
+    'user1' => 'city1',
+    'user2' => 'city2',
+    'user3' => 'city3',
+    'user4' => 'city4',
+    'user5' => 'city5',
+];
 
-	Route::get('/post/9', function () { //Ограничения можно накладывать на несколько параметров
-		return 'Девятый пост';
-	});
-
-	Route::get('/post/{id}', function ($id) { //метод ограничивает параметр только цифрами
-		
-	})->whereNumber('id');
-
-	Route::get('/post/{slug}', function ($slug) { //только буквами
-		
-	})->whereAlpha('slug');
-
-	Route::get('/post/{slug}', function ($slug) { //цифрами и буквами
-		
-	})->whereAlphaNumeric('slug');
-
-*/
-?>
-
-<?php
-Route::get('/test', function () { //выводилось какое-нибудь сообщение. №1
-	return 'Тест';
+Route::get('/user/{username}', function($username) use ($users) {
+    if (array_key_exists($username, $users)) {
+        return "Город пользователя: " . $users[$username];
+    } else {
+        return "Пользователь не найден";
+    }
 });
 
-Route::get('/dir/test ', function () { //выводилось какое-нибудь сообщение. №2
-	return 'Директория: тест';
-});
-
-Route::get('/user/{name}', function ($name) { //вместо :name может быть любая строка. №3
-
-})->whereAlpha('slug');
-
-Route::get('/post/{surname}/{name}', function ($surname, $name) { //параметры задают имя и фамилию юзера. №4
-	return 'Фамилия: ' . $surname . 'Имя:' . $name;
-})->where('$surname')->where('$name');
-
-Route::get('/city/{city?}', function ($city = "Омск") { //город был необязательным параметром и по умолчанию имел значение omsk. №5
-	return 'Город' . $city;
-});
-
-Route::get('/user/{id}', function ($id) { //маршрут вида /user/:id, где вместо :id должно быть число. №6
-	return 'пост ' . $id;
-})->where('id', '[0-9]+');
-
-Route::get('/user/{id}/{name},', function ($surname, $name) { //параметры задают имя и фамилию юзера. №7
-	return 'Фамилия: ' . $id . 'Имя:' . $name;
-})->where('name', '[a-z3-9_-]+')->where('id', '[0-9]+');
-
-Route::get('/{year}/{month}/{day},', function ($year, $month, $day) { //№8
-	return 'Год: ' . $year . 'месяц:' . $month . 'день:' . $day;
-})->where('year', '[0-9_-]+')->where('month', '[0-12]+')->where('day', '[0-9]+');
 ?>
