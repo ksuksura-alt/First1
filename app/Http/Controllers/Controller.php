e2ae1e9a4ff3da285d7bd16592a27322732c44b1
Ксения <ksura@mail.ru>
2026-03-18 00:09:32 -0700

    Лабораторная 2 готовая

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+class Controller extends Controller
 {
-    //
+    public function show()
+    {
+        return "Страница пользователя";
+    }
+
+    public function all()
+    {
+        return "Список всех пользователей";
+    }
+
+    public function showByName($name)
+    {
+        return "Пользователь: " . $name;
+    }
+
+    public function showByFullName($surname, $name)
+    {
+        return "Пользователь: $surname $name";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,68 @@
- <?php
-use App\Http\Controllers\UserController;
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+// Задание 1
+Route::get('/test', function () { //выводилось какое-нибудь сообщение. №1
+    return 'Тест';
+});
+
+// Задание 2
+Route::get('/dir/test', function () {
+    return 'Содержимое директории test';
+});
+
+// Задание 3
+Route::get('/user/{name}', function ($name) {
+    return 'Пользователь: ' . $name;
+});
+
+// Задание 4
+Route::get('/user/{surname}/{name}', function ($surname, $name) {
+    return 'Пользователь: ' . $name . ' ' . $surname;
+});
+
+// Задание 5
+Route::get('/city/{city?}', function ($city = 'omsk') {
+    return 'Город: ' . $city;
+});
+
+// Задание 6
+Route::get('/user/{id}', function ($id) {
+    return 'ID пользователя: ' . $id;
+})->where('id', '[0-9]+');
+
+// Задание 7
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return 'ID: ' . $id . ', Имя: ' . $name;
+})->where('id', '[0-9]+')->where('name', '[a-z]{3,}');
+
+// Задание 8
+Route::get('/posts/{date}', function ($date) {
+    return 'Дата: ' . $date;
+})->where('date', '[0-9]{4}-[0-9]{2}-[0-9]{2}');
+
+// Задание 9
+Route::get('/{year}/{month}/{day}', function ($year, $month, $day) {
+    return 'Дата: ' . $year . '-' . $month . '-' . $day;
+})->where('year', '[0-9]{4}')
+    ->where('month', '[0-9]{2}')
+    ->where('day', '[0-9]{2}');
+
+// Задание 10
+Route::get('/users/{order}', function ($order) {
+    return 'Сортировка по: ' . $order;
+})->where('order', '(name|surname|age)');
+
+// Задание 11
+Route::get('/city/{name}', function ($name) {
+    return 'Город: ' . $name;
+})->whereAlpha('name');
+
+
+/*use App\Http\Controllers\UserController;
 
 Route::get('/user', [UserController::class, 'show']);
 Route::get('/user/all', [UserController::class, 'all']);
@@ -20,6 +83,4 @@ Route::get('/user/{username}', function($username) use ($users) {
     } else {
         return "Пользователь не найден";
     }
-});
-
-?>
+});*/
